5e52993a58e75214c69027f389a2431265fe21a0
MariaP-Ramalho <101139658+MariaP-Ramalho@users.noreply.github.com>
2024-04-24 08:24:37 -0300

    eureka server update

---
+++ eureka-server-app/src/main/java/br/ucsal/eurekaserverapp/EurekaServerAppApplication.java
@@ -0,0 +1,16 @@
+package br.ucsal.eurekaserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class EurekaServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerAppApplication.class, args);
+    }
+
+}
---
+++ eureka-server-app/src/test/java/br/ucsal/eurekaserverapp/EurekaServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.eurekaserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
