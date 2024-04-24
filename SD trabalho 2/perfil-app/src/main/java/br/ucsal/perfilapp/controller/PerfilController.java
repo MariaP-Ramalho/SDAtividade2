1dd6edf7684f2adf9e8c483551be6d6ebd8cad5f
MariaP-Ramalho <101139658+MariaP-Ramalho@users.noreply.github.com>
2024-04-24 13:24:17 -0300

    update perfil-app

--- SD trabalho 2/perfil-app/src/main/java/br/ucsal/perfilapp/controller/PerfilController.java
+++ SD trabalho 2/perfil-app/src/main/java/br/ucsal/perfilapp/controller/PerfilController.java
@@ -2,6 +2,8 @@ package br.ucsal.perfilapp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -9,8 +11,8 @@ import java.time.LocalDateTime;
 @RestController
 public class PerfilController {
 
-    @GetMapping("/health")
-    public String healthy() {
+    @PostMapping("/perfil")
+    public String healthy(@RequestBody String payload) {
         return "Perfil Acessado ";
     }
 }
