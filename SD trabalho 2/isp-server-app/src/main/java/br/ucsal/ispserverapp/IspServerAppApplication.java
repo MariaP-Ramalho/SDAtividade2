696d36110e5f67e0b8ede177919c43beb7c7de33
MariaP-Ramalho <101139658+MariaP-Ramalho@users.noreply.github.com>
2024-04-24 08:44:37 -0300

    isp-server-app

--- SD trabalho 2/isp-server-app/src/main/java/br/ucsal/ispserverapp/IspServerAppApplication.java
+++ SD trabalho 2/isp-server-app/src/main/java/br/ucsal/ispserverapp/IspServerAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IspServerAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IspServerAppApplication.class, args);
 	}
 
