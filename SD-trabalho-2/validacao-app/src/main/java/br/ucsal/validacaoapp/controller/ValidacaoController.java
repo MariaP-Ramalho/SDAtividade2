db4ef7e62fe7212f1e49b32d97bae2e959e6de86
Ninoar3 <109094779+Ninoar3@users.noreply.github.com>
2024-04-26 20:24:04 -0300

    Motificação da Vallidacao controller

--- SD-trabalho-2/validacao-app/src/main/java/br/ucsal/validacaoapp/controller/ValidacaoController.java
+++ SD-trabalho-2/validacao-app/src/main/java/br/ucsal/validacaoapp/controller/ValidacaoController.java
@@ -1,27 +1,19 @@
 package br.ucsal.validacaoapp.controller;
 
-import com.netflix.discovery.EurekaClient;
-
-import br.ucsal.validacaoapp.services.IValidationService;
-import br.ucsal.validacaoapp.viewmodels.ValidacaoRequestDTO;
-import br.ucsal.validacaoapp.viewmodels.ValidacaoResponseDTO;
-
 import org.apache.hc.core5.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ucsal.validacaoapp.services.IValidationService;
+import br.ucsal.validacaoapp.viewmodels.ValidacaoRequestDTO;
+import br.ucsal.validacaoapp.viewmodels.ValidacaoResponseDTO;
+import br.ucsal.validacaoapp.viewmodels.NotFoundResponseDTO;
 
 @RestController
 public class ValidacaoController {
-    @Autowired
-    @Lazy
-    private EurekaClient eurekaClient;
 
     private final IValidationService service;
 
@@ -35,12 +27,14 @@ public class ValidacaoController {
     }
 
     @PostMapping("/validacao")
-    public ResponseEntity<ValidacaoResponseDTO> validateUser(@RequestBody ValidacaoRequestDTO dto) {
+    public ResponseEntity<?> validateUser(@RequestBody ValidacaoRequestDTO dto) {
         var message = service.validateUser(dto.getEmail());
         var responseDTO = new ValidacaoResponseDTO(message);
 
-        if(message.contains("não foi encontrado"))
-            return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body(responseDTO);
+        if (message.contains("não foi encontrado")) {
+            var errorMessage = new NotFoundResponseDTO("Usuário não encontrado para o email: " + dto.getEmail());
+            return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body(errorMessage);
+        }
 
         return ResponseEntity.ok(responseDTO);
     }
